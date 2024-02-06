1387074fb3fda30b3fa53da60efc3cf2d75899f4
Tucker-Evans-19 <evanst@mit.edu>
2024-02-06 11:50:28 -0500

    initial commit for all plasma analysis work

---
+++ readme.txt
@@ -0,0 +1,3 @@
+if you want to install this folder for everywhere then run 'pip install -e .'
+
+you might need to run setup.py before doing that
---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+setuptools.setup(
+    name = "plasma_analysis", 
+    version = "0.1.0", 
+    description = "Plasma scripts", 
+    long_description = "", 
+    packages = setuptools.find_packages()
+)
+
+
